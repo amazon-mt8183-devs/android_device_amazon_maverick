d4932bf55a10cb6a5fe0e3ed518331f6b9b14bf0
bengris32 <bengris32@protonmail.ch>
2022-02-28 13:11:37 +0000

    maverick: Initial device specific overlay
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Icc55372c95058187d5829221c1e68075fa7be925

--- device.mk
+++ device.mk
@@ -94,6 +94,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
