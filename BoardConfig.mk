277a34772abab366d22ef4144f75365fcdddb114
bengris32 <bengris32@protonmail.ch>
2023-02-23 18:29:37 +0000

    maverick: Define Architecture flags
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Id14678fb82ca9ed597d82a59fea96bcf8f4e7767

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,14 @@
 
 DEVICE_PATH := device/amazon/maverick
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+
 # Inherit the proprietary files
 include vendor/amazon/maverick/BoardConfigVendor.mk
