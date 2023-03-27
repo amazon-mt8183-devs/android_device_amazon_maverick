d21d7e657c88d860edffbb5d1410af8f897c1597
bengris32 <bengris32@protonmail.ch>
2023-03-27 13:59:08 +0100

    maverick: Switch to 64-bit userspace
    * Update architecture flags for 64-bit userspace.
    * Inherit core_64_bit.mk
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I4e1f099554ce0fc36bb2f27e5d69701e170abd05

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,13 +7,17 @@
 DEVICE_PATH := device/amazon/maverick
 
 # Architecture
-TARGET_ARCH := arm
+TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a73
 
-TARGET_USES_64_BIT_BINDER := true
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := maverick
--- lineage_maverick.mk
+++ lineage_maverick.mk
@@ -5,6 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from device makefile.
