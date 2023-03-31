6245bbee168578929c1118eb2240c66d3c9be40e
bengris32 <bengris32@protonmail.ch>
2023-03-31 19:55:58 +0100

    maverick: Import Media configs from `trona`
    * We use the OMX blobs from trona, so it only makes sense
      that we use the same configs too.
    
    * From trona-user 9 PS7327.3326N 0026743733760 amz-p,release-keys
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I34ae2e001a3baac0e15d554086c0d38e9122eaa9

--- device.mk
+++ device.mk
@@ -23,6 +23,14 @@ PRODUCT_COPY_FILES += \
 # Characteristics
 PRODUCT_CHARACTERISTICS := tablet
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.mt8183.rc \
