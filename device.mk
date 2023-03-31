60cfdb3f67c446b94213398a4bd8ccefb1224c56
bengris32 <bengris32@protonmail.ch>
2023-03-31 19:10:26 +0100

    maverick: Initial minimal rootdir from `trona`
    * init.mt8183.usb.rc, ueventd.mt8183.rc and init.project.rc are from maverick stock.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: If53532cd3b6ed96c312da3e69e76a14b475a0362

--- device.mk
+++ device.mk
@@ -4,6 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.mt8183.rc \
+    init.mt8183.usb.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    fstab.mt8183 \
+    ueventd.mt8183.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
