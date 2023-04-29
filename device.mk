bd7debd2c412f6861a83738ff2def07dadd9d3ba
bengris32 <bengris32@protonmail.ch>
2023-04-29 20:12:13 +0100

    maverick: Relocate audio configs
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: If65b5d95ecdee65cb9ef7c1f37762ceed9fa731d

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
     libxml2-v28
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
